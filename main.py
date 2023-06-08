c603fda4ebdbe3675513446d6f49a223abb9ed7f
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-06-08 11:38:07 +0300

    i have been able to create the menu, i know what to go a further step and try to save in a database

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
-
-# Creating the engine and connecting to the database
-engine = create_engine('sqlite:///car_dealership.db')
-Base = declarative_base(bind=engine)
-Session = sessionmaker(bind=engine)
-
-#defining the car class
-class Car(Base):
-    __tablename__ = 'car'
-    id = Column(Integer, primary_key=True)
-    make = Column(String)
-    model = Column(String)
-    year = Column(Integer)
-    price = Column(Integer)
-    manufacturer_id = Column(Integer, ForeignKey('manufacturer.id'))   
-    manufacturer = relationship("Manufacturer", back_populates="cars")
+def main():
     
-# Defining the Manufacturer table
-class Manufacturer(Base):
-    __tablename__ = 'manufacturer'
-    # Defining the primary key
-    # Defining the columns
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    #initialize carlist
+    carlist = []
     
-    # Creating the tables
-    Base.metadata.create_all(engine)
-    
-    # Creating a session
-    session = Session()
-    
-    # Creating a new car
-    new_car = Car(make="Ford", model="Mustang", year=1)
-    new_car = Car(make="Ford", model="Mustang", year=1)
-    
-
-   
-
-
+    # creating the car dealership menu
+    choice = 0
+    while choice != 4:
+        print("*** CARS MENU ***")
+        print("1. Order a car")
+        print("2. Check a car")
+        print("3. Print all cars")
+        print("4. Exit")
+        choice = int(input())
+        
+        if choice == 1:
+            print("You are ordering at Augasha Motors")
+            nbrand = input ("Brand of car: ")
+            nhorsepower = input ("HorsePower of car: ")
+            nwheels = input ("No of wheels of car: ")
+            ncolor = input ("Color of car: ") 
+            carlist.append([nbrand, nhorsepower, nwheels, ncolor])
+            
+        elif choice == 2:
+            print("You are checking a car at Augasha Motors")
+            keyword = input("search for a car: ")
+            for car in carlist:
+                if keyword in car:
+                    print(car)
+                    
+        elif choice == 3:
+            print("You are printing all cars at Augasha Motors")
+            for i in range(len(carlist)):
+                print(carlist[i])
+                
+        elif choice == 4:
+            print("You are exiting the car dealership")
+    print("Programme terminated, Thank you.")
+                    
+           
+if __name__ == "__main__":
+    main()
